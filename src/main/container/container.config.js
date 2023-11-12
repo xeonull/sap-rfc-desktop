1b1f6483f33663f05432f6460048b3b914c46e7c
xeonull <xeonull@gmail.com>
2023-11-12 10:48:49 +0300

    add fake rfc service

--- src/main/container/container.config.js
+++ src/main/container/container.config.js
@@ -1,11 +1,14 @@
 import { TOKENS } from './types.js';
 import { Container } from 'brandi';
-import { NodeRFC } from '../rfc/services/node-rfc.js';
+import { NodeRFC } from '../rfc/services/node-rfc';
+import { FakeRFC } from '../rfc/services/fake-rfc';
+import pkg from '../../../package.json';
 
 const container = new Container();
+
 container
   .bind(TOKENS.RFC_SERVICE)
-  .toInstance(NodeRFC) /* ← Binds the token to an instance */
+  .toInstance(pkg.debug.env.FAKE_RFC_SERVICE ? FakeRFC : NodeRFC) /* ← Binds the token to an instance */
   .inSingletonScope();
 
 export { container };
---
+++ src/main/rfc/services/fake-rfc/TABLE_UJA_APPSET_INFO.js
@@ -0,0 +1,20 @@
+export default {
+  ET_DATA: [{ LINE: 'ENVIRONMENTSHELL' }],
+  DELIMITER: ';',
+  GET_SORTED: '',
+  NO_DATA: '',
+  QUERY_TABLE: 'UJA_APPSET_INFO',
+  ROWCOUNT: 999999,
+  ROWSKIPS: 0,
+  USE_ET_DATA_4_RETURN: 'X',
+  FIELDS: [
+    {
+      FIELDNAME: 'APPSET_ID',
+      OFFSET: '000000',
+      LENGTH: '000020',
+      TYPE: 'C',
+      FIELDTEXT: '',
+    },
+  ],
+  OPTIONS: [],
+};
